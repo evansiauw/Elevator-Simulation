ad9e28cba07724ebe335dab50650bc20691aef8b
Iwan Siauw <evansiauw@gmail.com>
2018-04-23 12:07:44 -0400

    update

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,20 @@
+import java.util.Random;
+
 public class Controller {
 
 	public static void main(String[] args) {
 		
-		int maxRunningTime = 1; // in minute
+		int maxRunningTime = 60; // a minute
 		double time = 0; // in second
+		int j = 1;
+		int meanArrival = 5;
+		int personAt;
+		int destinationFloor;
+		
 		Elevator [] elevator = new Elevator[3];
 		Floor [] floor = new Floor[11];
+		Person [] person  = new Person[100];
+		Random rand = new Random();
 		
 		for(int i=1; i<=2; i++) {
 			elevator[i] = new Elevator(i);
@@ -15,6 +24,20 @@ public class Controller {
 			floor[i]= new Floor(i);
 		}
 		
+		while(time < maxRunningTime) {
+			double custArrival = meanArrival * (- Math.log(1 - rand.nextDouble()));
+			
+			do {
+			personAt = rand.nextInt(11) + 1;
+			destinationFloor = rand.nextInt() + 1;
+			}while (personAt == destinationFloor);
+			
+			person[j] = new Person(j,custArrival, personAt, destinationFloor);
+			
+			
+			
+			
+		}
 		
 		
 		
--- src/Person.java
+++ src/Person.java
@@ -2,8 +2,14 @@
 public class Person extends Controller{
 
 	int floorDestination;
+	int personNumber;
+	int personAt;
+	double arrivalTime;
 	
-	public Person(int floorDestination){
+	public Person(int personNumber, double arrivalTime, int personAt, int floorDestination){
+		this.personNumber = personNumber;
+		this.arrivalTime = arrivalTime;
+		this.personAt = personAt;
 		this.floorDestination = floorDestination;
 	}
 }
